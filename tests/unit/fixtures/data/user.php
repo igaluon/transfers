56bf17a8716de71abda6d46bde59cfcdf7e8d34e
igaluon <igaluon@gmail.com>
2017-11-16 23:49:13 +0200

    Данные фикстур

---
+++ tests/unit/fixtures/data/user.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+        'username' => 'admin',
+        'balance' => 200,
+        'auth_key' => 'bEde5l9DccLIQCds5WdFB-vW60Y-GSqW',
+        'created_at' => '1234567891',
+        'updated_at' => '1234567891',
+    ],
+    [
+        'username' => 'user',
+        'balance' => -200,
+        'auth_key' => 'kEde5l9DvvLIQCds5WdFB-vW60Y-GSqW',
+        'created_at' => '1509983778',
+        'updated_at' => '1509983791',
+    ],
+];
